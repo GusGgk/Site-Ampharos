fc6168f6c67f5242622b16f7f3e6ae26043cc048
GusGk <gukumagai@gmail.com>
2025-01-29 10:01:24 -0300

    port

--- server.py
+++ server.py
@@ -52,6 +52,8 @@ def remover_reserva(quadrado_id):
         conn.commit()
     return jsonify({"mensagem": "Reserva removida com sucesso!"})
 
+
 if __name__ == "__main__":
     criar_tabela()
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
