948bc2df98ba90a6731fbe290b8d6d76b9ad82d7
GusGk <gukumagai@gmail.com>
2025-01-30 15:57:11 -0300

    a

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Importar o CORS
 import json
-import os
 
 app = Flask(__name__)
 CORS(app)  # Ativar CORS para todas as rotas
@@ -52,6 +51,7 @@ def remover_reserva(quadrado_id):
     salvar_reservas(reservas)
     return jsonify({"message": "Reserva removida com sucesso!"}), 200
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
     app.run(host="0.0.0.0", port=5000, debug=True)
+
