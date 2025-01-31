345b423b6ded0c9a792fd36ab8aeeb0726c1948b
GusGk <gukumagai@gmail.com>
2025-01-31 16:25:34 -0300

    adicionando integração online do Render

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const infoBoxes = document.querySelectorAll(".info-box");
-
-  infoBoxes.forEach(box => {
-    box.addEventListener("click", toggleInfo);
-    box.addEventListener("touchstart", toggleInfo); // Adiciona suporte ao toque
-  });
-
-  function toggleInfo(event) {
-    event.preventDefault(); // Evita problemas com o toque duplo
-    this.classList.toggle("ativo");
-  }
-});
+setInterval(() => {
+    fetch("https://site-ampharos.onrender.com/ping", { method: "GET", cache: "no-store" })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Erro HTTP: ${response.status}`);
+        }
+        console.log("Ping enviado com sucesso!");
+      })
+      .catch(error => console.error("Erro ao enviar ping:", error));
+}, 600000); // 10 minutos
